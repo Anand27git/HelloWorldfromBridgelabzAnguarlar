46a1c4f0d0a86f06b7897d017395d98155fecc96
Anand27git <anandkumar256k@gmail.com>
2021-08-25 13:23:56 +0530

    Hello world from Bridge Labz with User input

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+/*------components-------------*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hello-world';
+  imgUrl;
+  url;
+  userName: string = "";
+  nameError: string = "";
+
+  ngOnInit(): void {
+    this.title = "Hello from BridgeLabz ";
+    this.imgUrl = "../assets/BL_logo_square_jpg.jpg";
+    this.url = "https://www.bridgelabz.com";
+  }
+
+  onClick($event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect!";
+  }
+}
